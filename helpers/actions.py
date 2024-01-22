e5c03dccf98f41eefa5ae94e8770c65043676e8d
dendy <lyovin.denis@gmail.com>
2024-01-23 04:56:06 +0500

    1.1.1

--- helpers/actions.py
+++ helpers/actions.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 def click_element(driver, xpath):
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-    element.click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
 
 
 def send_text(driver, xpath, text):
